ad5c4010c84ea105050e73f9fdbc354aae25a13e
Jessica Nygren <jessicanygren@C02D54BJMD6R.local>
2021-10-27 19:16:54 +0200

    card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ProfileImage from "./components/ProfileImage";
 import Header from "./components/Header";
+import Card from "./components/Card";
 import "./index.css";
 
 const App = () => {
@@ -10,16 +11,28 @@ const App = () => {
   const title = 'Get ready for adventure'
   const backgroundColor = { backgroundColor: '#151414ee'}
 
+  const cardTitle = 'The beginning of everything.'
+  const date = 'JANUARY 2022'
+  const number = '01'
+  const cardImage = './magic-pattern.png'
+
+
   return (
     <>
-    <Header 
-     title = {title}
-     backgroundColor = {backgroundColor}
-     background
+      <Header 
+      title = {title}
+      backgroundColor = {backgroundColor}
+      background
+        />
+      <section>
+        <ProfileImage image={image} textColor={textColor} name={name} />;
+      </section>
+      <Card 
+        cardImage = {cardImage}
+        cardTitle = {cardTitle}
+        date = {date}
+        number = {number}
       />
-    <section>
-      <ProfileImage image={image} textColor={textColor} name={name} />;
-    </section>
     </>
   );
 };
---
+++ src/components/Card.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Card = ({ cardTitle, cardImage, date, number }) => {
+  return (
+    <div className="card-container">
+        <div className='image-container'>
+            <img className='image' src={cardImage} alt="card-image" ></img>
+        </div>
+        <div className='text-container'>
+            <div className='date-number'>
+                <p>{date}</p>
+                <p>{number}</p>
+            </div>
+            <h3>{cardTitle}</h3>
+        </div>
+    </div>
+
+    
+  );
+};
+
+export default Card;
